4b34cc1ff4f1b658352f6a7bb24fbc84e300dbab
rezafauzan <rezafauzan593@gmail.com>
2026-04-07 17:27:50 +0700

    Feat: add new library to return postgres pool connection

---
+++ backend/internal/lib/db.go
@@ -0,0 +1,34 @@
+package lib
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func DatabaseConnect() (*pgxpool.Pool, error) {
+	dbURL := os.Getenv("DATABASE_URL")
+
+	var pool *pgxpool.Pool
+	var err error
+
+	for range 10 {
+		pool, err = pgxpool.New(context.Background(), dbURL)
+		if err == nil {
+			err = pool.Ping(context.Background())
+			if err == nil {
+				fmt.Println("Connection to database established!")
+				return pool, nil
+			}
+		}
+
+		fmt.Println("Database not ready, retrying...")
+		time.Sleep(4 * time.Second)
+	}
+
+	return nil, errors.New("could not connect to database: " + err.Error())
+}
