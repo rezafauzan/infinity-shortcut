11a9db7b45b372cba904e16bc265d756d62af6b4
rezafauzan <rezafauzan593@gmail.com>
2026-04-07 11:58:08 +0700

    Feat: split login component from AuthLayout

--- src/pages/auth/AuthLayout.jsx
+++ src/pages/auth/AuthLayout.jsx
@@ -1,44 +1,10 @@
-import { RiEyeLine, RiEyeCloseLine } from "react-icons/ri";
-import { AiOutlineArrowRight } from "react-icons/ai";
-import { Link } from "react-router-dom";
-import { useState } from "react";
-import { useForm } from "react-hook-form";
+import { Outlet } from "react-router-dom";
 const AuthLayout = () => {
-    const [isPasswordVisible, setIsPasswordVisible] = useState(false);
-    const { register, handleSubmit } = useForm()
-    function login({ email, password }) {
-        console.log(email, password)
-    }
     return (
         <>
             <section>
-                <div className="flex justify-center items-center gap-4 h-screen">
-                    <div className="flex flex-col justify-center gap-4 p-4 rounded shadow border border-black/10 min-w-lg">
-                        <div className="flex flex-col justify-center">
-                            <span className="font-bold text-lg">Welcome Back</span>
-                            <span>Please enter your details to sign in.</span>
-                        </div>
-                        <form onSubmit={handleSubmit(login)}>
-                            <div className="flex flex-col justify-center gap-4">
-                                <label for="email">Email Address</label>
-                                <label className="flex items-center gap-4 p-4 border rounded border-black/40">
-                                    <input type="email" {...register("email")} id="email" placeholder="name@snowfoxinfinity.com" className="flex-1" />
-                                </label>
-
-                                <div className="flex justify-between items-center gap-4">
-                                    <label for="password">Password</label>
-                                    <Link to="#">Forgot password?</Link>
-                                </div>
-
-                                <label className="flex items-center gap-4 p-4 border rounded border-black/40">
-                                    <input type={isPasswordVisible ? "text" : "password"} {...register("password")} id="password" placeholder={isPasswordVisible ? "password" : "*******"} className="flex-1" />
-                                    <button type="button" className="cursor-pointer" onClick={() => { setIsPasswordVisible(!isPasswordVisible) }}>{isPasswordVisible ? <RiEyeLine /> : <RiEyeCloseLine />}</button>
-                                </label>
-
-                                <button className="bg-blue-700 text-white py-2 flex justify-center items-center gap-4 rounded">Log In <AiOutlineArrowRight /></button>
-                            </div>
-                        </form>
-                    </div>
+                <div className="flex flex-col justify-center items-center gap-4 py-4 min-h-screen">
+                    <Outlet />
                 </div>
             </section >
         </>
---
+++ src/pages/auth/Login.jsx
@@ -0,0 +1,54 @@
+import { AiOutlineGoogle } from "react-icons/ai";
+import { RiEyeLine, RiEyeCloseLine } from "react-icons/ri";
+import { AiOutlineArrowRight } from "react-icons/ai";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+
+const Login = () => {
+    const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+    const { register, handleSubmit } = useForm()
+    function login({ email, password }) {
+        console.log(email, password)
+    }
+    return (
+        <>
+            <span className="text-xl font-bold">Infinity Shortcut</span>
+            
+            <div className="flex flex-col justify-center gap-4 p-4 rounded shadow border border-black/10 min-w-lg">
+                <div className="flex flex-col justify-center">
+                    <span className="font-bold text-lg">Welcome Back</span>
+                    <span>Please enter your details to sign in.</span>
+                </div>
+                <form onSubmit={handleSubmit(login)}>
+                    <div className="flex flex-col justify-center gap-4">
+                        <label for="email">Email Address</label>
+                        <label className="flex items-center gap-4 p-4 border rounded border-black/40">
+                            <input type="email" {...register("email")} id="email" placeholder="name@snowfoxinfinity.com" className="flex-1" />
+                        </label>
+                        <div className="flex justify-between items-center gap-4">
+                            <label for="password">Password</label>
+                            <Link to="#" className="text-blue-700 hover:text-blue-900">Forgot password?</Link>
+                        </div>
+                        <label className="flex items-center gap-4 p-4 border rounded border-black/40">
+                            <input type={isPasswordVisible ? "text" : "password"} {...register("password")} id="password" placeholder={isPasswordVisible ? "password" : "*******"} className="flex-1" />
+                            <button type="button" className="cursor-pointer" onClick={() => { setIsPasswordVisible(!isPasswordVisible) }}>{isPasswordVisible ? <RiEyeLine /> : <RiEyeCloseLine />}</button>
+                        </label>
+                        <button className="bg-blue-700 hover:bg-blue-800 text-white py-2 flex justify-center items-center gap-4 rounded cursor-pointer">Log In <AiOutlineArrowRight /></button>
+                    </div>
+                </form>
+                <div className="flex items-center gap-4">
+                    <div className="grow border-t border-gray-400"></div>
+                    <span className="text-gray-400">OR CONTINUE WITH</span>
+                    <div className="grow border-t border-gray-400"></div>
+                </div>
+                <div className="flex justify-center items-center px-4">
+                    <button type="button" className="hover:bg-black/10 py-2 border-black/10 flex-1 flex justify-center items-center rounded shadow cursor-pointer"><AiOutlineGoogle />Sign in with Google</button>
+                </div>
+            </div>
+
+            <span>Don't have an account? <Link to="/auth/register" className="text-blue-700 hover:text-blue-900">Sign up</Link></span>
+        </>
+    )
+}
+export default Login
