7a14c1eadd1e81f56d1f4763d5f0b6b31310f065
rezafauzan <rezafauzan593@gmail.com>
2026-04-08 00:35:34 +0700

    Feat: add new link handler

---
+++ backend/internal/handlers/link.handler.go
@@ -0,0 +1,58 @@
+package handlers
+
+import (
+	"net/http"
+	"snowfoxinfinity/infinity-shortcut/internal/dto"
+	"snowfoxinfinity/infinity-shortcut/internal/services"
+
+	"github.com/gin-gonic/gin"
+)
+
+type LinkHandler struct {
+	linkService *services.LinkService
+}
+
+func NewLinkHandler(linkService *services.LinkService) *LinkHandler {
+	return &LinkHandler{
+		linkService: linkService,
+	}
+}
+
+// CreateNewLink godoc
+// @Summary      Create new shortened link
+// @Description  Create new shortened link
+// @Tags         links
+// @Accept json
+// @Produce json
+// @Param body body dto.CreateNewLinkDTO true "Registration payload"
+// @Success 201 {object} dto.Response{data=dto.CreateNewLinkResponseDTO}
+// @Failure      400 {object} dto.Response
+// @Failure      500 {object} dto.Response
+// @Router       /api/link [post]
+func (l LinkHandler) CreateNewLink(ctx *gin.Context) {
+	var newLinkData dto.CreateNewLinkDTO
+	err := ctx.ShouldBind(&newLinkData)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, dto.ResponseDTO{
+			Success: false,
+			Message: "Create new link failed! " + err.Error(),
+			Data:    nil,
+		})
+		return
+	}
+
+	registeredUser, err := l.linkService.CreateNewLink(newLinkData)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, dto.ResponseDTO{
+			Success: false,
+			Message: "Internal server error" + err.Error(),
+			Data:    nil,
+		})
+		return
+	}
+	ctx.JSON(http.StatusCreated, dto.ResponseDTO{
+		Success: true,
+		Message: "Register success!",
+		Data:    registeredUser,
+	})
+}
