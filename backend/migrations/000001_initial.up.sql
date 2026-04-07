eec328f837c2273eadd2ffd9c535f36867f34cab
rezafauzan <rezafauzan593@gmail.com>
2026-04-07 20:02:18 +0700

    Feat: add suffix _hash on password column name

--- backend/migrations/000001_initial.up.sql
+++ backend/migrations/000001_initial.up.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS users (
     first_name VARCHAR(255) NOT NULL,
     last_name VARCHAR(255) NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
     created_at TIMESTAMP DEFAULT NOW(),
     updated_at TIMESTAMP
 )
