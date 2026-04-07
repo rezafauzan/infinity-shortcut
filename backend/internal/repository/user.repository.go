b4fb5758a94a4cd5983dcd6764e3aa35a53225b9
rezafauzan <rezafauzan593@gmail.com>
2026-04-07 17:55:24 +0700

    Feat: add new user repository has CreateNewUser

---
+++ backend/internal/repository/user.repository.go
@@ -0,0 +1,36 @@
+package repository
+
+import (
+	"context"
+	"errors"
+	"snowfoxinfinity/infinity-shortcut/internal/models"
+	"time"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type UserRepository struct {
+	db *pgxpool.Pool
+}
+
+func NewUserRepository(db *pgxpool.Pool) (*UserRepository, error) {
+	return &UserRepository{
+		db: db,
+	}, nil
+}
+
+func (u UserRepository) CreateNewUser(newUser models.User) (models.User, error) {
+	sql := "INSERT INTO users (name, email, password, created_at, updated_at) VALUES ($1, $2, $3, $4, $5) RETURNING id, name, email, password, created_at, updated_at"
+	rows, err := u.db.Query(context.Background(), sql, newUser.Name, newUser.Email, newUser.Password, time.Now(), time.Now())
+	if err != nil {
+		return models.User{}, errors.New("Failed to create new user! : " + err.Error())
+	}
+
+	registeredUser, err := pgx.CollectOneRow(rows, pgx.RowToStructByName[models.User])
+	if err != nil {
+		return models.User{}, errors.New("User created but returning error! : " + err.Error())
+	}
+
+	return registeredUser, nil
+}
