71c2a4050835125be6578e45604054bd317aa006
rezafauzan <rezafauzan593@gmail.com>
2026-04-08 03:31:24 +0700

    Feat: new AuthMiddleware

---
+++ backend/internal/middleware/auth.middleware.go
@@ -0,0 +1,55 @@
+package middleware
+
+import (
+	"net/http"
+	"snowfoxinfinity/infinity-shortcut/internal/dto"
+	"snowfoxinfinity/infinity-shortcut/internal/lib"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
+
+func AuthMiddleware() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+
+		authHeader := ctx.GetHeader("Authorization")
+		if authHeader == "" {
+			ctx.JSON(http.StatusUnauthorized, dto.ResponseDTO{
+				Success: false,
+				Message: "Authorization header required",
+				Data:    nil,
+			})
+			ctx.Abort()
+			return
+		}
+		
+		if !strings.HasPrefix(authHeader, "Bearer ") {
+			ctx.JSON(http.StatusUnauthorized, dto.ResponseDTO{
+				Success: false,
+				Message: "Invalid token format",
+				Data:    nil,
+			})
+			ctx.Abort()
+			return
+		}
+
+		tokenString := strings.TrimPrefix(authHeader, "Bearer ")
+
+		claims, err := lib.VerifyJWT(tokenString)
+		
+		if err != nil {
+			ctx.JSON(http.StatusUnauthorized, dto.ResponseDTO{
+				Success: false,
+				Message: "Invalid or expired token",
+				Data:    nil,
+			})
+			ctx.Abort()
+			return
+		}
+
+		ctx.Set("user_id", int(claims.UserId))
+		ctx.Set("cart_id", int(claims.CartId))
+		ctx.Set("role", "user")
+		ctx.Next()
+	}
+}
