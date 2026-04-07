543bf21c2fda76252acbc67c1d97fc5d0337e1a4
rezafauzan <rezafauzan593@gmail.com>
2026-04-07 14:09:05 +0700

    Feat: add new route / to access Homepage

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,19 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import AuthLayout from "./pages/auth/AuthLayout"
 import Login from "./pages/auth/Login"
 import Register from "./pages/auth/Register"
+import Home from "./pages/Home/Home"
 
 function App() {
   const router = createBrowserRouter(
     [
       {
+        path: '/',
+        element: <Home />
+      },
+      {
         path: '/auth',
         element: <AuthLayout />,
-        children:[
+        children: [
           {
             path: 'login',
             element: <Login />
@@ -17,12 +22,12 @@ function App() {
           {
             path: 'register',
             element: <Register />
-          },
+          }
         ]
-
       }
     ]
   )
+  
   return (
     <RouterProvider router={router} />
   )
