009223d22f2e280c9ad0ee5d49db7b58453eaa2c
rezafauzan <rezafauzan593@gmail.com>
2026-04-08 05:21:24 +0700

    Feat: add DeleteLinkResponseDTO

--- backend/internal/dto/link.dto.go
+++ backend/internal/dto/link.dto.go
@@ -15,11 +15,21 @@ type CreateNewLinkResponseDTO struct {
 	UpdatedAt   time.Time `json:"updated_at"`
 }
 
-type GetAllLinksResponseDTO struct {
+type GetLinkResponseDTO struct {
 	Id          int       `json:"id"`
 	UserId      int       `json:"user_id"`
 	OriginalUrl string    `json:"original_url"`
 	ShortUrl    string    `json:"short_url"`
 	CreatedAt   time.Time `json:"created_at"`
 	UpdatedAt   time.Time `json:"updated_at"`
+}
+
+type DeleteLinkResponseDTO struct {
+	Id          int       `json:"id"`
+	UserId      int       `json:"user_id"`
+	OriginalUrl string    `json:"original_url"`
+	ShortUrl    string    `json:"short_url"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+	DeletedAt   time.Time `json:"deleted_at"`
 }
