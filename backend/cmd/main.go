01b34582808345ea1cb4b9ed6c0a0d604ce4d26c
rezafauzan <rezafauzan593@gmail.com>
2026-04-07 22:53:25 +0700

    Feat: add container and router to main.go

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"os"
+	"snowfoxinfinity/infinity-shortcut/internal/di"
+	"snowfoxinfinity/infinity-shortcut/internal/routers"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
@@ -13,5 +15,13 @@ func main() {
 	godotenv.Load()
 
 	router := gin.Default()
-	router.Run(fmt.Sprintf(":%s", os.Getenv("PORT")))
+	container, err := di.NewContainer()
+
+	if err != nil {
+		panic("Container Error : " + err.Error())
+	}
+	apiRouter := router.Group("/api")
+	routers.NewUserRouters(apiRouter, container)
+
+	router.Run(fmt.Sprintf("localhost:%s", os.Getenv("PORT")))
 }
