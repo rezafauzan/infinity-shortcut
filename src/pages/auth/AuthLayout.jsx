ee16089179c9ccca41cfc45c762df77dce370650
rezafauzan <rezafauzan593@gmail.com>
2026-04-07 15:19:36 +0700

    Feat: add footer in AuthLayout

--- src/pages/auth/AuthLayout.jsx
+++ src/pages/auth/AuthLayout.jsx
@@ -1,4 +1,5 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../../components/Footer";
 const AuthLayout = () => {
     return (
         <>
@@ -7,6 +8,7 @@ const AuthLayout = () => {
                     <Outlet />
                 </div>
             </section >
+            <Footer />
         </>
     )
 }
