3c29be860dfdb035d2a9eb550ef4b9d1a34271e1
rezafauzan <rezafauzan593@gmail.com>
2026-04-08 07:49:10 +0700

    Feat: add alert system on App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,8 +3,18 @@ import AuthLayout from "./pages/auth/AuthLayout"
 import Login from "./pages/auth/Login"
 import Register from "./pages/auth/Register"
 import Home from "./pages/Home/Home"
+import AlertContext from "/src/components/context/AlertContext"
+import { useRef, useState } from "react"
+import { AiOutlineCloseCircle } from "react-icons/ai"
 
 function App() {
+  const [alert, setAlert] = useState([])
+  const modal = useRef()
+
+  function modalRemove() {
+    setAlert([])
+  }
+  
   const router = createBrowserRouter(
     [
       {
@@ -27,9 +37,13 @@ function App() {
       }
     ]
   )
-  
+
   return (
-    <RouterProvider router={router} />
+    <AlertContext value={{ setAlert }}>
+      {(alert[0] === "success" ? <div ref={modal} className="fixed top-0 left-0 right-0 bottom-0 bg-black/40 backdrop-blur-lg flex justify-center items-center z-10"><div className="bg-green-400 text-green-700 w-[50%] h-[50%] flex items-center justify-center relative rounded"><button type="button" className="text-red-700 w-10 h-10 absolute -top-4 -right-4 cursor-pointer" onClick={modalRemove}><AiOutlineCloseCircle className="text-red-700 w-10 h-10" /></button><span className="text-green-700 p-4 font-bold">{alert[1]}</span></div></div> : "")}
+      {(alert[0] === "fail" ? <div ref={modal} className="fixed top-0 left-0 right-0 bottom-0 bg-black/40 backdrop-blur-lg flex justify-center items-center z-10"><div className="bg-red-400 text-red-700 w-[50%] h-[50%] flex items-center justify-center relative rounded"><button type="button" className="text-red-700 w-10 h-10 absolute -top-4 -right-4 cursor-pointer" onClick={modalRemove}><AiOutlineCloseCircle className="text-red-700 w-10 h-10" /></button><span className="text-red-700 p-4 font-bold">{alert[1]}</span></div></div> : "")}
+      <RouterProvider router={router} />
+    </AlertContext>
   )
 }
 
