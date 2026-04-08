a2df491e16620e5f5b3895760a1db4fbdda18f9e
rezafauzan <rezafauzan593@gmail.com>
2026-04-08 07:49:54 +0700

    Feat: integrating backend register with frontend register

--- frontend/src/pages/auth/Register.jsx
+++ frontend/src/pages/auth/Register.jsx
@@ -1,17 +1,47 @@
 import { RiEyeLine, RiEyeCloseLine } from "react-icons/ri";
 import { AiOutlineArrowRight } from "react-icons/ai";
-import { Link } from "react-router-dom";
-import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useContext, useState } from "react";
 import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup"
+import AlertContext from "/src/components/context/AlertContext"
 import logo from "/assets/img/drawable/logo.svg"
+import http from "../../lib/http";
 
 const Register = () => {
     const [isPasswordVisible, setIsPasswordVisible] = useState(false);
     const [isConfirmPasswordVisible, setIsConfirmPasswordVisible] = useState(false);
-    const { register, handleSubmit } = useForm()
-    function signup({ email, password }) {
-        console.log(email, password)
+    const { setAlert } = useContext(AlertContext)
+    const navigator = useNavigate()
+    const validator = yup.object({
+        first_name: yup.string("Nama depan tidak valid").required("Nama depan harus diisi").min(4, "Nama depan minimal 4 karakter"),
+        last_name: yup.string("Nama belakang tidak valid").required("Nama belakang harus diisi").min(4, "Nama belakang minimal 4 karakter"),
+        email: yup.string("Email tidak valid").required("Email harus diisi").min(4, "Email terlalu pendek").email("Email tidak valid"),
+        password: yup.string("Password tidak valid").required("Password harus diisi").min(8, "Password minimal 8 karakter"),
+        confirm_password: yup.string("Konfirmasi Password tidak valid").required("Konfirmasi Password harus diisi").oneOf([yup.ref("password")], "Konfirmasi Password tidak sama")
+    })
+    const { register, handleSubmit, formState } = useForm({ resolver: yupResolver(validator) })
+
+    async function signup({ first_name, last_name, email, password, confirm_password }) {
+        try {
+            const req = await http("register", { first_name, last_name, email, password, confirm_password }, { method: "POST" })
+            const result = await req.json()
+            if (!result.success) {
+                throw new Error(result.message)
+            }
+            setAlert(['success', result.message + " will be redirected to login page in 4 seconds"])
+            setTimeout(
+                () => {
+                    setAlert(['success', result.message + " will be redirected to login page in 4 seconds"])
+                    navigator("/auth/login")
+                }, 4000
+            )
+        } catch (error) {
+            setAlert(['fail', error.message])
+        }
     }
+
     return (
         <>
             <img src={logo} alt="Infinity Shortcut" />
@@ -25,26 +55,46 @@ const Register = () => {
                 </div>
                 <form onSubmit={handleSubmit(signup)}>
                     <div className="flex flex-col justify-center gap-4">
+                        <div className="flex justify-between items-center gap-4">
+                            <div className="flex flex-col justify-center gap-4">
+                                <label htmlFor="first_name">First Name</label>
+                                <label className="flex items-center gap-4 p-4 border rounded border-black/40">
+                                    <input type="text" {...register("first_name")} id="first_name" placeholder="Reza" className="flex-1" />
+                                </label>
+                                {formState.errors.first_name && (<span className="bg-red-400 p-4 rounded border border-red-700 text-red-700">{formState.errors.first_name.message}</span>)}
+                            </div>
+                            <div className="flex flex-col justify-center gap-4">
+                                <label htmlFor="last_name">Last Name</label>
+                                <label className="flex items-center gap-4 p-4 border rounded border-black/40">
+                                    <input type="text" {...register("last_name")} id="last_name" placeholder="Fauzan" className="flex-1" />
+                                </label>
+                                {formState.errors.last_name && (<span className="bg-red-400 p-4 rounded border border-red-700 text-red-700">{formState.errors.last_name.message}</span>)}
+                            </div>
+                        </div>
                         <label htmlFor="email">Email Address</label>
                         <label className="flex items-center gap-4 p-4 border rounded border-black/40">
                             <input type="email" {...register("email")} id="email" placeholder="name@snowfoxinfinity.com" className="flex-1" />
                         </label>
+                        {formState.errors.email && (<span className="bg-red-400 p-4 rounded border border-red-700 text-red-700">{formState.errors.email.message}</span>)}
+
                         <div className="flex justify-between items-center gap-4">
                             <label htmlFor="password">Password</label>
                         </div>
                         <label className="flex items-center gap-4 p-4 border rounded border-black/40">
-                            <input type={isPasswordVisible ? "text" : "password"} {...register("password")} id="password" placeholder={isPasswordVisible ? "password" : "*******"} className="flex-1" autoComplete="off"/>
+                            <input type={isPasswordVisible ? "text" : "password"} {...register("password")} id="password" placeholder={isPasswordVisible ? "password" : "*******"} className="flex-1" autoComplete="off" />
                             <button type="button" className="cursor-pointer" onClick={() => { setIsPasswordVisible(!isPasswordVisible) }}>{isPasswordVisible ? <RiEyeLine /> : <RiEyeCloseLine />}</button>
                         </label>
                         <span className="-mt-4 text-gray-700">Minimum 8 characters</span>
+                        {formState.errors.password && (<span className="bg-red-400 p-4 rounded border border-red-700 text-red-700">{formState.errors.password.message}</span>)}
 
                         <div className="flex justify-between items-center gap-4">
                             <label htmlFor="confirm-password">Confirm Password</label>
                         </div>
                         <label className="flex items-center gap-4 p-4 border rounded border-black/40">
-                            <input type={isConfirmPasswordVisible ? "text" : "password"} {...register("confirm_password")} id="confirm-password" placeholder={isConfirmPasswordVisible ? "confirm password" : "*******"} className="flex-1" autoComplete="off"/>
+                            <input type={isConfirmPasswordVisible ? "text" : "password"} {...register("confirm_password")} id="confirm-password" placeholder={isConfirmPasswordVisible ? "confirm password" : "*******"} className="flex-1" autoComplete="off" />
                             <button type="button" className="cursor-pointer" onClick={() => { setIsConfirmPasswordVisible(!isConfirmPasswordVisible) }}>{isConfirmPasswordVisible ? <RiEyeLine /> : <RiEyeCloseLine />}</button>
                         </label>
+                        {formState.errors.confirm_password && (<span className="bg-red-400 p-4 rounded border border-red-700 text-red-700">{formState.errors.confirm_password.message}</span>)}
                         <button className="bg-blue-700 hover:bg-blue-800 text-white py-2 flex justify-center items-center gap-4 rounded cursor-pointer">Sign Up <AiOutlineArrowRight /></button>
                     </div>
                 </form>
